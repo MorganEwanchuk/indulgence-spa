f2a1dfb56cfd0a4815373e3de9659813888fed63
Morgan <mewanchuk@live.com>
2022-09-18 13:49:08 -0600

    changed spacing on all items

--- server.js
+++ server.js
@@ -15,22 +15,22 @@ app.post('/myForm', (req, res) => {
     const { header,consentName, secondConsentName, header3, techniquesParagraph, listItem1, listItem2, listItem3, listItem4, listItem5, listItem6} = req.body
 
     const techniquesItems = [listItem1,listItem2,listItem3,listItem4,listItem5,listItem6];
-    
+
     const doc = new PDFDocument();
     doc.pipe(fs.createWriteStream('output.pdf'))
 
-    doc.fontSize(20).text(header);
+    doc.fontSize(20).text(header, {marginBottom: 7}).moveDown();
     // doc.fontSize(14).text(paragraph1);
     // doc.fontSize(14).text(paragraph2);
-    doc.fontSize(16).text(header3);
+    doc.fontSize(16).text(header3, {marginBottom: 7}).moveDown();
 
-    doc.fontSize(14).text(techniquesParagraph);
-    doc.list(techniquesItems)
-
-
+    doc.fontSize(14).text(techniquesParagraph, {marginBottom: 7}).moveDown();
+    doc.list(techniquesItems, {marginBottom: 7}).moveDown();
 
 
-    doc.fontSize(12).text(`I, ${consentName}, have read and understand the above information and give my consent to the registered massage therapist of Indulgence Therapeutic Massage to treat me with the understanding that the above regions indicated above may be touched for therapeutic purposes. `);
+
+
+    doc.fontSize(12).text(`I, ${consentName}, have read and understand the above information and give my consent to the registered massage therapist of Indulgence Therapeutic Massage to treat me with the understanding that the above regions indicated above may be touched for therapeutic purposes.`, {marginBottom: 7}).moveDown();
     doc.fontSize(12).text(`I, ${secondConsentName}, understand that at any point of the massage session, I can request my therapist to adjust/change the pressure and techniques used.`);
 
 
