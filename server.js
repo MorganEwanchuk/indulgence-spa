4aebb54f2fc83edcf37bb26832c91cc2e75db5bd
Morgan <mewanchuk@live.com>
2022-09-25 22:22:17 -0600

    updated pdf to email function

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const PDFDocument = require('pdfkit');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 const nodemailer = require('nodemailer')
@@ -12,6 +14,18 @@ app.get('/', (req, res) => {
 app.post('/myForm', (req, res) => {
     const { header,consentName, secondConsentName, paragraph1, paragraph2, header3} = req.body
 
+    const doc = new PDFDocument();
+    doc.pipe(fs.createWriteStream('output.pdf'))
+
+    doc.fontSize(20).text(header);
+    doc.fontSize(14).text(paragraph1);
+    doc.fontSize(14).text(paragraph2);
+    doc.fontSize(16).text(header3);
+    doc.fontSize(12).text(`Name: ${consentName}`);
+    doc.fontSize(12).text(`Second Name: ${secondConsentName}`);
+
+    doc.end()
+
     const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
@@ -19,20 +33,35 @@ app.post('/myForm', (req, res) => {
           pass: 'dexppxlrqbomyhdh'
         }
       });
+
       const mailOptions = {
         from: 'morganlieu@gmail.com',
         to: 'mewanchuk@live.com',
         subject: 'New form submission',
-        html: `
-            
-          <h1>${header}</h1>
-          <p>${paragraph1}</p>
-          <p>${paragraph2}</p>
-          <h3>${header3}</h3>
-          <p>Name: ${consentName}</p>
-          <p>Second Name: ${secondConsentName}</p>
-        `
+        attachments: [{
+            filename: 'output.pdf',
+            path: 'output.pdf',
+            contentType: 'application/pdf',
+            encoding: 'utf8'
+        }]
       };
+
+
+
+    //   const mailOptions = {
+    //     from: 'morganlieu@gmail.com',
+    //     to: 'mewanchuk@live.com',
+    //     subject: 'New form submission',
+    //     html: `
+            
+    //       <h1>${header}</h1>
+    //       <p>${paragraph1}</p>
+    //       <p>${paragraph2}</p>
+    //       <h3>${header3}</h3>
+    //       <p>Name: ${consentName}</p>
+    //       <p>Second Name: ${secondConsentName}</p>
+    //     `
+    //   };
       transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
           console.log(error);
